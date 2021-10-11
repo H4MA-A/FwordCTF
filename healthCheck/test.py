799590721b09c217a4b38eb6361dc93cf68db030
Mohamed Arfaoui <mohamed.arfaoui@insat.u-carthage.tn>
2021-10-11 13:12:58 +0100

    Update and rename test.py to README.md

--- healthCheck/test.py
+++
@@ -1,4 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-webhook = DiscordWebhook(url='https://discord.com/api/webhooks/877707596855910490/uqAInqGESUybVebz_LLxuNp-yzP5hXD7kAFFPQ2ZGn2Nl3xjlY4IYHCSzeE-jsT7C_Fv', content='Webhook Message')
-response = webhook.execute()
